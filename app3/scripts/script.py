dd1f94e233619cd1b3a5b4e914e532747e0ac786
Samy0010 <samy.robert15@gmail.com>
2024-03-28 13:50:43 +0200

    test commit

--- app3/scripts/script.py
+++ app3/scripts/script.py
@@ -7,18 +7,14 @@ import requests
 
 app = Flask(__name__, template_folder='/app')
 
-# Endpoint to receive data via POST requests
 @app.route('/submit_form', methods=['POST'])
 def send_data():
-     # Assuming JSON data is sent
     data = {
         'name': request.form['fname'],
         'age': request.form['age']
     }
-    json_data = jsonify(data)
     headers={'Content-Type': 'application/json'}
-    # Send data to Server B
-    response = requests.post('http://server1:5000/receive_data', json=json_data, headers=headers)
+    response = requests.post('http://server1:5000/receive_data', json=data, headers=headers)
     return response.text
 
 @app.route('/')
